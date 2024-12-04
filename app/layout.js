6c6322decb5beef64b8dffecbe1206229e5abd54
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-12-04 08:33:24 +0530

    feat: improved navigation and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,12 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "./_components/Header";
+import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+
 const josefin = Josefin_Sans({
   subsets: ["latin"],
   display: "swap",
 });
-console.log(josefin);
-import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: {
@@ -22,13 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
