c85886d3745784261e599dd3b071156ca6024d40
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-12-05 23:15:25 +0530

    feat: data integration in cabin list

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.SUPABASE_URL.replace('https://', ''),
+        port: '',
+        pathname: `${process.env.SUPABASE_CABIN_IMAGES_PATHNAME}/**`,
+      },
+    ],
+  },
+};
 
 export default nextConfig;
