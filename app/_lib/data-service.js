1838aaf1d839cc218ac983f8a0992611665e2c00
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-12-15 00:30:57 +0530

    feat: implemented ISR and caching

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -40,7 +40,8 @@ export async function getCabinPrice(id) {
 export const getCabins = async function () {
   const { data, error } = await supabase
     .from('cabins')
-    .select('id, name, maxCapacity, regularPrice, discount, image');
+    .select('id, name, maxCapacity, regularPrice, discount, image')
+    .order('name');
 
   if (error) {
     console.error(error);
