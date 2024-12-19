6fa0868b856f5aadefb6f878788fbdbef2e8b357
Onkareshwar Prasad <onkareshwarkupra@gmail.com>
2024-12-19 13:18:36 +0530

    feat: added filter functionality to cabin list

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,16 +2,34 @@
 import CabinCard from '@/app/_components/CabinCard';
 import { getCabins } from '@/app/_lib/data-service';
 
-async function CabinList() {
+async function CabinList({ filter }) {
   // noStore();
 
   const cabins = await getCabins();
 
   if (!cabins.length) return null;
 
+  let filteredCabins;
+  switch (filter) {
+    case 'small':
+      filteredCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+      break;
+    case 'medium':
+      filteredCabins = cabins.filter(
+        (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+      );
+      break;
+    case 'large':
+      filteredCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+      break;
+    default:
+      filteredCabins = cabins;
+      break;
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
